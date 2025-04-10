c020281b0538fdc66cf8dabd827245ab8cdf28dd
Lalramnunmawia <80774787+papuia13@users.noreply.github.com>
2025-04-10 13:01:44 +0530

    adding NUMBER_OF_ADULTS and NUMBER_OF_CHILDREN in AnnexeBookingKeysPBinder

--- src/main/java/in/edu/kristujayanti/propertyBinder/KJAnnexe/AnnexeBookingKeysPBinder.java
+++ src/main/java/in/edu/kristujayanti/propertyBinder/KJAnnexe/AnnexeBookingKeysPBinder.java
@@ -9,19 +9,22 @@ public enum AnnexeBookingKeysPBinder implements KJUSYSPropertyBinder {
     BOOKING_OBJECT_ID("bookingObjectId_KJAnnexeBooking_ObjectId", "MongoDB ObjectID", ObjectId.class),
     OBJECT_ID("_id", "MongoDB ObjectID", ObjectId.class),
     BOOKING_TYPE("bookingType_KJAnnexeBooking_Text", "Booking Type", String.class),
-    //Guest Details
+    // Guest Details
     GUEST_INFO("guestInfo_KJAnnexeBooking_DocumentArray", "Guest Information", Document.class),
-    BED_NO ("bedNumber_KJAnnexeRoom_Int","Bed NO.",ObjectId.class), //if booked by bed only
+    BED_NO("bedNumber_KJAnnexeRoom_Int", "Bed NO.", Integer.class),
     GUEST_NAME("guestName_KJAnnexeBooking_Text", "Guest Name", String.class),
-    GUEST_GENDER("guestGender_KJAnnexeBooking_Text","Guest Gender",String.class),
-    GUEST_AADHAAR_NO("guestAadhaarNo_KJAnnexeBooking_Text","Guest Aadhaar No",String.class),
-    GUEST_PHONE_NO("guestPhoneNo_KJAnnexeBooking_Text","Guest Phone No",String.class),
-    GUEST_DESIGNATION("guestDesignation_KJAnnexeBooking_Text","Guest Designation",String.class),
-    GUEST_ADDRESS("guestAddress_KJAnnexeBooking_Text","Guest Address",String.class),
-    GUEST_EMAIL_ID("guestEmailId_KJAnnexeBooking_Text","Guest Email", String.class),
-
+    GUEST_GENDER("guestGender_KJAnnexeBooking_Text", "Guest Gender", String.class),
+    GUEST_AADHAAR_NO("guestAadhaarNo_KJAnnexeBooking_Text", "Guest Aadhaar No", String.class),
+    GUEST_PHONE_NO("guestPhoneNo_KJAnnexeBooking_Text", "Guest Phone No", String.class),
+    GUEST_DESIGNATION("guestDesignation_KJAnnexeBooking_Text", "Guest Designation", String.class),
+    GUEST_ADDRESS("guestAddress_KJAnnexeBooking_Text", "Guest Address", String.class),
+    GUEST_EMAIL_ID("guestEmailId_KJAnnexeBooking_Text", "Guest Email", String.class),
+    // New Fields with NUMBER_OF_ prefix
+    NUMBER_OF_ADULTS("numberOfAdults_KJAnnexeBooking_Int", "Number of Adults", Integer.class),
+    NUMBER_OF_CHILDREN("numberOfChildren_KJAnnexeBooking_Int", "Number of Children", Integer.class),
+    // Other Fields
     PURPOSE_OF_VISIT("purposeOfVisit_KJAnnexeBooking_Text", "Purpose of Visit", String.class),
-    EVENT_DEPARTMENT("eventDepartment_KJAnnexeBooking_Text","Event Host by Department",String.class),
+    EVENT_DEPARTMENT("eventDepartment_KJAnnexeBooking_Text", "Event Host by Department", String.class),
     STAFF_IN_CHARGE("staffInCharge_KJAnnexeBooking_Text", "Staff In Charge", String.class),
     STAFF_IN_CHARGE_MOBILE("staffInChargeMobile_KJAnnexeBooking_Text", "Staff In Charge Mobile", String.class),
     PROGRAMME_NAME("programmeName_KJAnnexeBooking_Text", "Programme Name", String.class),
@@ -30,11 +33,11 @@ public enum AnnexeBookingKeysPBinder implements KJUSYSPropertyBinder {
     CHECK_IN_DATE("checkInDate_KJAnnexeBooking_Date", "Check-in Date", Long.class),
     CHECK_OUT_DATE("checkOutDate_KJAnnexeBooking_Date", "Check-out Date", Long.class),
     COLLEGE_GUEST("collegeGuest_KJAnnexeBooking_Bool", "College Guest", Boolean.class),
-    //Room Details
+    // Room Details
     SELECTED_ROOM("selectedRoom_KJAnnexeBooking_DocumentArray", "Selected Room Details", Document.class),
     SELECTED_ROOM_NO("selectedRoomNo_KJAnnexeBooking_Int", "Selected Room Number", Integer.class),
     SELECTED_ROOM_TYPE("selectedRoomType_KJAnnexeBooking_Text", "Selected Room Type", String.class),
-    SELECTED_BED_NO("selectedBedNo_KJAnnexeBooking_Int","Numbers of Bed Selected",Integer.class),
+    SELECTED_BED_NO("selectedBedNo_KJAnnexeBooking_Int", "Numbers of Bed Selected", Integer.class),
     IS_MAIN_GUEST("isMainGuest_KJAnnexeBooking_Bool", "Main Guest", Boolean.class),
     BOOKING_STATUS("bookingStatus_KJAnnexeBooking_Text", "Booking Status", String.class),
     PAYMENT_STATUS("paymentStatus_KJAnnexeBooking_Bool", "Payment Status", Boolean.class),
@@ -86,4 +89,3 @@ public enum AnnexeBookingKeysPBinder implements KJUSYSPropertyBinder {
         return AnnexeBookingKeysPBinder.class.getEnumConstants();
     }
 }
-
