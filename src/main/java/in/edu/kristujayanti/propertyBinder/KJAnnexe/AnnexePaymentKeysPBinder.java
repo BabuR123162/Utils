ff91469ff5509b97bf7b9a1524f2e12a87dabee0
AnkithaPradosh <23mscs04@kristujayanti.com>
2025-05-12 09:53:02 +0530

    paid amount added

--- src/main/java/in/edu/kristujayanti/propertyBinder/KJAnnexe/AnnexePaymentKeysPBinder.java
+++ src/main/java/in/edu/kristujayanti/propertyBinder/KJAnnexe/AnnexePaymentKeysPBinder.java
@@ -14,6 +14,7 @@ public enum AnnexePaymentKeysPBinder implements KJUSYSPropertyBinder {
 //  PAYMENT_STATUS("paymentStatus_KJAnnexePayment_Text", "Payment Status", String.class),
     AMOUNT_PAID("amountPaid_KJAnnexePayment_Double", "Amount Paid", Double.class),
     PAYMENT_DATE("paymentDate_KJAnnexePayment_Date", "Payment Date", Long.class),
+    PAID_AMOUNT("paidAmount_KJAnnexePayment_Double", "Paid Amount", Double.class),
     BALANCE_DUE_AMOUNT("balanceDueAmount_KJAnnexePayment_Double", "Balance Due Amount", Double.class);
 
     private final String property;
